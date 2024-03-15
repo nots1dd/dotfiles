3cb8ee36c62fb2ccc9d4cd76f68df3701e7dc85c
nots1dd <sid9.karanam@gmail.com>
2024-03-15 23:11:52 +0530

    feat: init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+My Ricing so far
+
+Current Config is using:
+1. Hyprland
+2. Hyprlock
+3. swww (wallpaper daemon)
+4. wlogout
+5. waybar (can also use ags)
+6. swaync
+
+Other Configs:
+Terminal -> `kitty`
+Shell -> `zsh (oh my zsh)`
+music -> `cmus / spotify (with spicetify)`
---
+++ scripts/ss_fullscreen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+case $1 in
+    f)  hyprpicker -r -z & 
+    sleep 0.2
+    HYPRPICKER_PID=$!
+    BOUNDS="$(slurp -o -r -c '#ff0000ff')"
+    kill $HYPRPICKER_PID
+    grim -g "$BOUNDS" /tmp/unsaved.png
+    wl-copy < /tmp/unsaved.png
+    EDIT="$(dunstify -a "Skreenshot" --icon=/tmp/unsaved.png -A Y,Edit "Screenshot copied! 🎉")"
+    case $EDIT in
+        Y) satty --filename /tmp/unsaved.png --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png --copy-command wl-copy
+        exit;;
+    esac
+    exit;;
+
+    a) hyprpicker -r -z & 
+    sleep 0.2
+    HYPRPICKER_PID=$!
+    BOUNDS="$(slurp -c '#ff0000ff')"
+    kill $HYPRPICKER_PID
+    grim -g "$BOUNDS" /tmp/unsaved.png
+    wl-copy < /tmp/unsaved.png
+    EDIT="$(dunstify -a "Skreenshot" --icon=/tmp/unsaved.png -A Y,Edit "Screenshot copied! 🎉")"
+    case $EDIT in
+        Y) satty --filename /tmp/unsaved.png --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png --copy-command wl-copy
+        exit;;
+    esac    
+    exit;;
+esac
+
