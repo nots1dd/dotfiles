6cebfc270e9026de580ff1eac83eb6aee84eaffc
nots1dd <sid9.karanam@gmail.com>
2024-04-21 19:48:15 +0530

    feat: ags.sh & hypr config

---
+++ scripts/ags.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+show_msg() {
+    rofi -e "$1" -theme "/home/s1dd/Downloads/rofi/files/launchers/type-3/style-5.rasi"
+}
+
+
+# Check if the process "ags" is running
+if pgrep -x "ags" > /dev/null
+then
+    # If it's running, kill it
+    show_msg "AGS LAUNCHER :: ags process found. Esc to Quit ags" && ags -q
+    
+    # Sleep for 1 second
+    sleep 1
+fi
+
+# Relaunch ags
+show_msg "AGS LAUNCHER :: Press Esc to relaunch ags"
+ags
