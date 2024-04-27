828b97df39c9bb2f717cca897a4357846a28bfc9
nots1dd <sid9.karanam@gmail.com>
2024-04-27 19:59:02 +0530

    chore: rofi/script fix

--- scripts/clipboard.sh
+++ scripts/clipboard.sh
@@ -1,3 +1,3 @@
-cliphist list | rofi -dmenu -theme "~/dotfiles/rofi/ai-clipboard-config.rasi" | cliphist decode | wl-copy
+cliphist list | rofi -dmenu -theme "~/dotfiles/rofi/aiClipboard/ai-clipboard-config.rasi" | cliphist decode | wl-copy
 
 # can use clipboard.rasi as well but this better
