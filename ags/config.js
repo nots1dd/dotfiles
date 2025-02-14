cb04249f5c3181fdc9ae224be41f2f69720ac3a8
nots1dd <sid9.karanam@gmail.com>
2025-02-14 17:54:06 +0530

    [MAJOR] Dots revamp

--- ags/config.js
+++ ags/config.js
@@ -1,46 +1,81 @@
-import GLib from "gi://GLib"
+"use strict";
+// Import
+import Gdk from 'gi://Gdk';
+import GLib from 'gi://GLib';
+import App from 'resource:///com/github/Aylur/ags/app.js'
+import * as Utils from 'resource:///com/github/Aylur/ags/utils.js'
+// Stuff
+import userOptions from './modules/.configuration/user_options.js';
+import { firstRunWelcome, startBatteryWarningService } from './services/messages.js';
+import { startAutoDarkModeService } from './services/darkmode.js';
+// Widgets
+import { Bar, BarCornerTopleft, BarCornerTopright } from './modules/bar/main.js';
+import Cheatsheet from './modules/cheatsheet/main.js';
+// import DesktopBackground from './modules/desktopbackground/main.js';
+import Dock from './modules/dock/main.js';
+import Corner from './modules/screencorners/main.js';
+import Crosshair from './modules/crosshair/main.js';
+import Indicator from './modules/indicators/main.js';
+import Osk from './modules/onscreenkeyboard/main.js';
+import Overview from './modules/overview/main.js';
+import Session from './modules/session/main.js';
+import SideLeft from './modules/sideleft/main.js';
+import SideRight from './modules/sideright/main.js';
+import { COMPILED_STYLE_DIR } from './init.js';
 
-const main = "/tmp/ags/main.js"
-const entry = `${App.configDir}/main.ts`
-const bundler = GLib.getenv("AGS_BUNDLER") || "bun"
-
-const v = {
-    ags: pkg.version?.split(".").map(Number) || [],
-    expect: [1, 8, 0],
+const range = (length, start = 1) => Array.from({ length }, (_, i) => i + start);
+function forMonitors(widget) {
+    const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
+    return range(n, 0).map(widget).flat(1);
+}
+function forMonitorsAsync(widget) {
+    const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
+    return range(n, 0).forEach((n) => widget(n).catch(print))
 }
 
-try {
-    switch (bundler) {
-        case "bun": await Utils.execAsync([
-            "bun", "build", entry,
-            "--outfile", main,
-            "--external", "resource://*",
-            "--external", "gi://*",
-            "--external", "file://*",
-        ]); break
+// Start stuff
+handleStyles(true);
+startAutoDarkModeService().catch(print);
+firstRunWelcome().catch(print);
+startBatteryWarningService().catch(print)
 
-        case "esbuild": await Utils.execAsync([
-            "esbuild", "--bundle", entry,
-            "--format=esm",
-            `--outfile=${main}`,
-            "--external:resource://*",
-            "--external:gi://*",
-            "--external:file://*",
-        ]); break
+const Windows = () => [
+    // forMonitors(DesktopBackground),
+    forMonitors(Crosshair),
+    Overview(),
+    forMonitors(Indicator),
+    forMonitors(Cheatsheet),
+    SideLeft(),
+    SideRight(),
+    forMonitors(Osk),
+    forMonitors(Session),
+    ...(userOptions.dock.enabled ? [forMonitors(Dock)] : []),
+    ...(userOptions.appearance.fakeScreenRounding !== 0 ? [
+        forMonitors((id) => Corner(id, 'top left', true)),
+        forMonitors((id) => Corner(id, 'top right', true)),
+        forMonitors((id) => Corner(id, 'bottom left', true)),
+        forMonitors((id) => Corner(id, 'bottom right', true)),
+    ] : []),
+    ...(userOptions.appearance.barRoundCorners ? [
+        forMonitors(BarCornerTopleft),
+        forMonitors(BarCornerTopright),
+    ] : []),
+];
 
-        default:
-            throw `"${bundler}" is not a valid bundler`
-    }
-
-    if (v.ags[1] < v.expect[1] || v.ags[2] < v.expect[2]) {
-        print(`my config needs at least v${v.expect.join(".")}, yours is v${v.ags.join(".")}`)
-        App.quit()
-    }
-
-    await import(`file://${main}`)
-} catch (error) {
-    console.error(error)
-    App.quit()
+const CLOSE_ANIM_TIME = 210; // Longer than actual anim time to make sure widgets animate fully
+const closeWindowDelays = {}; // For animations
+for (let i = 0; i < (Gdk.Display.get_default()?.get_n_monitors() || 1); i++) {
+    closeWindowDelays[`osk${i}`] = CLOSE_ANIM_TIME;
 }
 
-export { }
+App.config({
+    css: `${COMPILED_STYLE_DIR}/style.css`,
+    stackTraceOnError: true,
+    closeWindowDelay: closeWindowDelays,
+    windows: Windows().flat(1),
+});
+
+// Stuff that don't need to be toggled. And they're async so ugh...
+forMonitorsAsync(Bar);
+// Bar().catch(print); // Use this to debug the bar. Single monitor only.
+
---
+++ fish/config.fish
@@ -0,0 +1,70 @@
+set -g theme_display_git yes
+set -g theme_display_git_dirty no
+set -g theme_display_git_untracked yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_display_git_default_branch yes
+set -g theme_git_default_branches master main
+set -g theme_git_worktree_support yes
+set -g theme_use_abbreviated_branch_name no
+set -g theme_display_vagrant yes
+set -g theme_display_docker_machine no
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv yes
+set -g theme_display_nix no
+set -g theme_display_ruby no
+set -g theme_display_node yes
+set -g theme_display_user ssh
+set -g theme_display_hostname ssh
+set -g theme_display_vi no
+set -g theme_display_date yes
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_title_display_path yes
+set -g theme_title_display_user yes
+set -g theme_title_use_abbreviated_path no
+set -g theme_date_format "+%a %H:%M:%S"
+set -g theme_date_timezone Asia/Calcutta
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose no
+set -g theme_color_scheme gruvbox
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '>> '
+set -g fish_prompt_pwd_dir_length 0
+
+if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
+    cat ~/.cache/ags/user/generated/terminal/sequences.txt
+end
+
+zoxide init fish | source
+
+alias pamcan=pacman
+alias cd=z
+alias nn=nvim
+alias playultra="mangohud gamemoderun DRI_PRIME=1 ~/Games/ULTRAKILL.Build\ 14344626/ULTRAKILL/ULTRAKILL.exe"
+alias lg=lazygit
+alias cat=bat
+alias :q=exit
+
+set -x EDITOR nvim
+set -x VISUAL swappy
+
+function sudo
+    set -l password (gum input --password --placeholder "Enter password" \
+        --header "  What you upto?" \
+        --header.foreground "#fabd2f" \
+        --prompt.foreground "#83a598" \
+        --cursor.foreground "#96D4A3")
+    echo $password | command sudo -S $argv
+end
+
+# function fish_prompt
+#   set_color cyan; echo (pwd)
+#   set_color green; echo '> '
+# end
+source /home/s1dd/.config/fish/completions/inLimbo.fish
